b2e7aae6777579194b8c22d7fe32262a6f691e60
erdi.erkmen <erdi.erkmen@trendyol.com>
2025-03-05 12:29:57 +0300

    refactor: improve error handling and logging in OutboxDispatcher

--- src/Coordinators/Services/OutboxDispatcher.cs
+++ src/Coordinators/Services/OutboxDispatcher.cs
@@ -26,134 +26,112 @@ public class OutboxDispatcher : IOutboxDispatcher
         IOptions<WorkerSettings> workerSettings,
         IMissingEventRepository missingEventRepository)
     {
-            _logger = logger;
-            _kafkaService = kafkaService;
-            _benchmarkOptions = benchmarkOptions;
-            _missingEventRepository = missingEventRepository;
-            _kafkaErrorsMaxRetryCount = workerSettings.Value.BrokerErrorsMaxRetryCount;
-            _redeliveryDelayAfterError = workerSettings.Value.RedeliveryDelayAfterError;
-        }
+        _logger = logger;
+        _kafkaService = kafkaService;
+        _benchmarkOptions = benchmarkOptions;
+        _missingEventRepository = missingEventRepository;
+        _kafkaErrorsMaxRetryCount = workerSettings.Value.BrokerErrorsMaxRetryCount;
+        _redeliveryDelayAfterError = workerSettings.Value.RedeliveryDelayAfterError;
+    }
 
     [Transaction]
     public async Task DispatchAsync(OutboxEvent outboxEvent)
     {
-            try
-            {
-                await PublishAsync(outboxEvent);
-                _logger.LogInformation("Event published. OutboxId: {OutboxId}", outboxEvent.Id);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Message broker is unavailable. Exception: {Exception}", ex.Message);
-
-                //move to missing events with has exception
-                await HandleGenericException(outboxEvent);
-            }
+        try
+        {
+            await PublishAsync(outboxEvent);
+            _logger.LogInformation("Event published. OutboxId: {OutboxId}", outboxEvent.Id);
         }
-
-    [Trace]
-    private async Task HandleGenericException(OutboxEvent outboxEvent)
-    {
-            try
-            {
-                var missingEvent = outboxEvent.ToMissingEvent();
-                missingEvent.ExceptionThrown = true;
-                await _missingEventRepository.InsertAsync(missingEvent);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error occured when trying to handle generic exception. Exception: {Exception}",
-                    ex.Message);
-            }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Message broker is unavailable. Exception: {Exception}", ex.Message);
+            await HandleExceptionSafe(() => HandleGenericException(outboxEvent));
         }
+    }
 
     [Transaction]
     public async Task<MappedMissingEvent> DispatchMissingAsync(MappedMissingEvent mappedMissingEvent)
     {
-            try
-            {
-                await PublishAsync(mappedMissingEvent.OutboxEvent);
-
-                // Set ExceptionThrown as false, and later this will be removed from missing events
-                mappedMissingEvent.MissingEvent.ExceptionThrown = false;
-                _logger.LogInformation("Event published. OutboxId: {OutboxId}", mappedMissingEvent.OutboxEvent.Id);
-            }
-            catch (MessageBrokerUnavailableException ex)
-            {
-                _logger.LogError(ex, "Message broker is unavailable. Exception: {Exception}", ex.Message);
-                HandleBrokerUnavailableException(mappedMissingEvent);
-            }
-            catch (MessageBrokerDeliveryFailedException ex)
-            {
-                _logger.LogError(ex, "Message broker delivery failed. Exception: {Exception}", ex.Message);
-                await HandleDeliveryException(mappedMissingEvent);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "An error occurred. Exception: {Exception}", ex.Message);
-                await HandleGenericExceptionForMissing(mappedMissingEvent);
-            }
-
-            return mappedMissingEvent;
+        try
+        {
+            await PublishAsync(mappedMissingEvent.OutboxEvent);
+            mappedMissingEvent.MissingEvent.ExceptionThrown = false;
+            _logger.LogInformation("Event published. OutboxId: {OutboxId}", mappedMissingEvent.OutboxEvent.Id);
         }
+        catch (MessageBrokerUnavailableException ex)
+        {
+            await HandleExceptionWithLog(ex, "Message broker is unavailable",
+                () => HandleBrokerUnavailableException(mappedMissingEvent));
+        }
+        catch (MessageBrokerDeliveryFailedException ex)
+        {
+            await HandleExceptionWithLog(ex, "Message broker delivery failed",
+                () => HandleMissingEventUpdate(mappedMissingEvent, true));
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionWithLog(ex, "An error occurred",
+                () => HandleMissingEventUpdate(mappedMissingEvent, false));
+        }
+
+        return mappedMissingEvent;
+    }
 
     [Trace]
     private async Task PublishAsync(OutboxEvent outboxEvent)
     {
-            if (!_benchmarkOptions.Value.IsPublishingOn)
-            {
-                _logger.LogInformation("Publishing closed. IsPublishingOn: {IsPublishingOn}",
-                    _benchmarkOptions.Value.IsPublishingOn);
-                return;
-            }
-
-            var kafkaModel = outboxEvent.PrepareKafkaMessageModel(outboxEvent.Topic, outboxEvent.Key);
-            await _kafkaService.ProduceAsync(kafkaModel.TopicName, kafkaModel.Message);
+        if (!_benchmarkOptions.Value.IsPublishingOn)
+        {
+            _logger.LogInformation("Publishing closed. IsPublishingOn: {IsPublishingOn}",
+                _benchmarkOptions.Value.IsPublishingOn);
+            return;
         }
 
-    private static void HandleBrokerUnavailableException(MappedMissingEvent mappedMissingEvent)
+        var kafkaModel = outboxEvent.PrepareKafkaMessageModel(outboxEvent.Topic, outboxEvent.Key);
+        await _kafkaService.ProduceAsync(kafkaModel.TopicName, kafkaModel.Message);
+    }
+
+    private async Task HandleGenericException(OutboxEvent outboxEvent)
     {
-            mappedMissingEvent.MissingEvent.ExceptionThrown = true;
-        }
+        var missingEvent = outboxEvent.ToMissingEvent();
+        missingEvent.ExceptionThrown = true;
+        await _missingEventRepository.InsertAsync(missingEvent);
+    }
 
-    [Trace]
-    private async Task HandleDeliveryException(MappedMissingEvent mappedMissingEvent)
+    private static Task HandleBrokerUnavailableException(MappedMissingEvent mappedMissingEvent)
     {
-            try
-            {
-                if (mappedMissingEvent.MissingEvent.IsRetryCountLessThan(_kafkaErrorsMaxRetryCount))
-                {
-                    mappedMissingEvent.MissingEvent.RetryCount += 1;
-                    mappedMissingEvent.MissingEvent.ExceptionThrown = true;
-                    await _missingEventRepository.UpdateRetryCountAndExceptionThrownAsync(mappedMissingEvent
-                        .MissingEvent);
-                    await Task.Delay(_redeliveryDelayAfterError);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error occured when trying to handle delivery exception. Exception: {Exception}",
-                    ex.Message);
-            }
-        }
+        mappedMissingEvent.MissingEvent.ExceptionThrown = true;
+        return Task.CompletedTask;
+    }
 
-    [Trace]
-    private async Task HandleGenericExceptionForMissing(MappedMissingEvent mappedMissingEvent)
+    private async Task HandleMissingEventUpdate(MappedMissingEvent mappedMissingEvent, bool shouldDelay)
     {
-            try
-            {
-                if (mappedMissingEvent.MissingEvent.IsRetryCountLessThan(_kafkaErrorsMaxRetryCount))
-                {
-                    mappedMissingEvent.MissingEvent.RetryCount += 1;
-                    mappedMissingEvent.MissingEvent.ExceptionThrown = true;
-                    await _missingEventRepository.UpdateRetryCountAndExceptionThrownAsync(mappedMissingEvent
-                        .MissingEvent);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error occured when trying to handle generic exception. Exception: {Exception}",
-                    ex.Message);
-            }
+        if (!mappedMissingEvent.MissingEvent.IsRetryCountLessThan(_kafkaErrorsMaxRetryCount))
+            return;
+
+        mappedMissingEvent.MissingEvent.RetryCount += 1;
+        mappedMissingEvent.MissingEvent.ExceptionThrown = true;
+        await _missingEventRepository.UpdateRetryCountAndExceptionThrownAsync(mappedMissingEvent.MissingEvent);
+
+        if (shouldDelay)
+            await Task.Delay(_redeliveryDelayAfterError);
+    }
+
+    private async Task HandleExceptionSafe(Func<Task> handler)
+    {
+        try
+        {
+            await handler();
         }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred while handling exception: {Exception}", ex.Message);
+        }
+    }
+
+    private async Task HandleExceptionWithLog(Exception ex, string message, Func<Task> handler)
+    {
+        _logger.LogError(ex, "{message}. Exception: {Exception}", message, ex.Message);
+        await HandleExceptionSafe(handler);
+    }
 }
